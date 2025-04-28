0c1924c708adfad586b9cd4f7c3468a2d27abb6a
Venkataramana <itsmepvr@debian.lan>
2025-04-27 23:55:02 -0400

    force a software GL fallback

--- src/xtend/cli.py
+++ src/xtend/cli.py
@@ -1,6 +1,11 @@
 import argparse
 from xtend import start_application
 
+import os
+os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --disable-software-rasterizer"
+from PyQt5.QtCore import Qt, QCoreApplication
+QCoreApplication.setAttribute(Qt.AA_UseSoftwareOpenGL)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", choices=["qt","web"], default="qt")
