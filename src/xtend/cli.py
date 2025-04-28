2b6752d08c4da0f8d95f17a02560927a8b4f7b1c
Venkataramana <itsmepvr@debian.lan>
2025-04-28 14:06:55 -0400

    fallback test

--- src/xtend/cli.py
+++ src/xtend/cli.py
@@ -1,10 +1,10 @@
 import argparse
 from xtend import start_application
 
-import os
-os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --disable-software-rasterizer"
-from PyQt5.QtCore import Qt, QCoreApplication
-QCoreApplication.setAttribute(Qt.AA_UseSoftwareOpenGL)
+# import os
+# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --disable-software-rasterizer"
+# from PyQt5.QtCore import Qt, QCoreApplication
+# QCoreApplication.setAttribute(Qt.AA_UseSoftwareOpenGL)
 
 def main():
     parser = argparse.ArgumentParser()
